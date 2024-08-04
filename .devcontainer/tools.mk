10b660bfe7123f7fa9daebbe4d8def68f9643e89
Alex Carney <alcarneyme@gmail.com>
2024-08-04 16:08:08 +0100

    Use a devcontainer

---
+++ .devcontainer/tools.mk
@@ -0,0 +1,121 @@
+ARCH ?= $(shell arch)
+BIN ?= $(HOME)/.local/bin
+
+ifeq ($(strip $(ARCH)),)
+$(error Unable to determine platform architecture)
+endif
+
+NODE_VERSION := 18.20.4
+
+# The versions of Python we support
+PYXX_versions := 3.12
+PY_INTERPRETERS =
+
+# Hatch is not only used for building packages, but bootstrapping any missing
+# interpreters
+HATCH ?= $(or $(shell command -v hatch), $(BIN)/hatch)
+
+$(HATCH):
+	curl -L --output /tmp/hatch.tar.gz https://github.com/pypa/hatch/releases/latest/download/hatch-$(ARCH)-unknown-linux-gnu.tar.gz
+	tar -xf /tmp/hatch.tar.gz -C /tmp
+	rm /tmp/hatch.tar.gz
+
+	test -d $(BIN) || mkdir -p $(BIN)
+	mv /tmp/hatch $(HATCH)
+
+	$@ --version
+	touch $@
+
+# This effectively defines a function `PYXX` that takes a Python version number
+# (e.g. 3.8) and expands it out into a common block of code that will ensure a
+# verison of that interpreter is available to be used.
+#
+# The is perhaps a bit more complicated than I'd like, but it should mean that
+# the project's makefiles are useful both inside and outside of a devcontainer.
+#
+# `PYXX` has the following behavior:
+# - If possible, it will reuse the user's existing version of Python
+#   i.e. $(shell command -v pythonX.X)
+#
+# - The user may force a specific interpreter to be used by setting the
+#   variable when running make e.g. PYXX=/path/to/pythonX.X make ...
+#
+# - Otherwise, `make` will use `$(HATCH)` to install the given version of
+#   Python under `$(BIN)`
+#
+# See: https://www.gnu.org/software/make/manual/html_node/Eval-Function.html
+define PYXX =
+
+PY$(subst .,,$1) ?= $$(shell command -v python$1)
+
+ifeq ($$(strip $$(PY$(subst .,,$1))),)
+
+PY$(subst .,,$1) := $$(BIN)/python$1
+
+$$(PY$(subst .,,$1)): $$(HATCH)
+	$$(HATCH) python find $1 || $$(HATCH) python install $1
+	ln -s $$$$($$(HATCH) python find $1) $$@
+
+	$$@ --version
+	touch $$@
+
+endif
+
+PY_INTERPRETERS += $$(PY$(subst .,,$1))
+endef
+
+# Uncomment the following line to see what this expands into.
+#$(foreach version,$(PYXX_versions),$(info $(call PYXX,$(version))))
+$(foreach version,$(PYXX_versions),$(eval $(call PYXX,$(version))))
+
+# Set a default `python` command if there is not one already
+PY ?= $(shell command -v python3)
+
+ifeq ($(strip $(PY)),)
+PY := $(BIN)/python
+
+$(PY): $(PY312)
+	ln -s $< $@
+	$@ --version
+	touch $@
+endif
+
+PY_INTERPRETERS += $(PY)
+#$(info $(PY_INTERPRETERS))
+
+
+PY_TOOLS := $(HATCH)
+
+# Node JS
+NPM ?= $(shell command -v npm)
+NPX ?= $(shell command -v npx)
+
+ifeq ($(strip $(NPM)),)
+
+NPM := $(BIN)/npm
+NPX := $(BIN)/npx
+NODE := $(BIN)/node
+NODE_DIR := $(HOME)/.local/node
+
+$(NPM):
+	curl -L --output /tmp/node.tar.xz https://nodejs.org/dist/v$(NODE_VERSION)/node-v$(NODE_VERSION)-linux-x64.tar.xz
+	tar -xJf /tmp/node.tar.xz -C /tmp
+	rm /tmp/node.tar.xz
+
+	[ -d $(NODE_DIR) ] || mkdir -p $(NODE_DIR)
+	mv /tmp/node-v$(NODE_VERSION)-linux-x64/* $(NODE_DIR)
+
+	[ -d $(BIN) ] || mkdir -p $(BIN)
+	ln -s $(NODE_DIR)/bin/node $(NODE)
+	ln -s $(NODE_DIR)/bin/npm $(NPM)
+	ln -s $(NODE_DIR)/bin/npx $(NPX)
+
+	$(NODE) --version
+	PATH=$(BIN) $(NPM) --version
+	PATH=$(BIN) $(NPX) --version
+
+endif
+
+# One command to bootstrap all tools and check their versions
+tools: $(PY_INTERPRETERS) $(PY_TOOLS) $(NPM)
+	for prog in $^ ; do echo -n "$${prog}\t" ; PATH=$(BIN) $${prog} --version; done
--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-.PHONY: html preview
+include .devcontainer/tools.mk
+
 
 ABLOG_ARGS=
 BUILDDIR=_build/html
@@ -6,17 +7,24 @@ PORT=8000
 
 default: html
 
+node_modules/.installed: package.json package-lock.json $(NPM)
+	$(NPM) ci
+	touch $@
+
+.PHONY: tailwind
+tailwind: node_modules/.installed
+
 # Rebuild everything if any JavaScript is modified
 $(BUILDDIR)/_static/js/theme.js: _static/js/theme.js
 	$(eval ABLOG_ARGS=-a)
 
 # Run tailwind if any styles are changed.
-_static/css/styles.css: styles.css tailwind.config.js $(wildcard _templates/*.html) _static/js/theme.js
-	tailwindcss -i styles.css -o _static/css/styles.css
+_static/css/styles.css: styles.css tailwind.config.js $(wildcard _templates/*.html) _static/js/theme.js tailwind
+	$(NPX) tailwindcss -i styles.css -o _static/css/styles.css
 	$(eval ABLOG_ARGS=-a)
 
 html: _static/css/styles.css $(BUILDDIR)/_static/js/theme.js
-	ablog build $(ABLOG_ARGS) -w $(BUILDDIR) -d _build/doctrees
+	$(HATCH) run 'blog:build'
 	mkdir -p $(BUILDDIR)/talks/
 	cp -r talks/introducing-esbonio $(BUILDDIR)/talks/introducing-esbonio
 	patch -N -p1 < searchtools.patch
