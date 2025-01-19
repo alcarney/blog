ea05aa7a549153c929efbbd0bf952501580593e9
Alex Carney <alcarneyme@gmail.com>
2025-01-19 18:03:30 +0000

    Detect blog posts based on blog tag
    When writing the file to disk the `DenoteHTMLBuilder` will reproduce
    the previous `blog/<year>/<title>` url scheme

--- extensions/denote.py
+++ extensions/denote.py
@@ -1,15 +1,23 @@
-"""denote.py - Add support for denote style file names for my notes."""
+"""denote.py - Add support for denote style file names.
+
+This is used to power the entire site, based on the metadata blog posts are
+generated, notes created etc.
+"""
 
 from __future__ import annotations
 
+import dataclasses
 import pathlib
+import re
 import typing
+from datetime import datetime
 
 from docutils import nodes
 from sphinx.application import Sphinx
 from sphinx.builders.dirhtml import DirectoryHTMLBuilder
 from sphinx.domains import Domain, ObjType
 from sphinx.roles import XRefRole
+from sphinx.util.logging import getLogger
 from sphinx.util.nodes import make_refnode
 
 if typing.TYPE_CHECKING:
@@ -18,18 +26,62 @@ if typing.TYPE_CHECKING:
     from sphinx.builders import Builder
     from sphinx.environment import BuildEnvironment
 
+logger = getLogger('denote')
+FILENAME_PATTERN = re.compile(r"(?P<identifier>\d{8}T\d{6})--(?P<title>[^_]+)__(?P<tags>[^.]+)", re.VERBOSE)
 
-def parse_denote_name(name: str) -> tuple[str, str, str] | None:
-    """Given a docname, parse the relevant denote file name components, if possible."""
-    docpath = pathlib.Path(name)
+@dataclasses.dataclass
+class Record:
+    """Represents a 'record' with a denote style filename."""
 
-    if "--" not in (docname := docpath.stem):
-        return None
+    identifier: str
+    slug: str
 
-    date, rest = docname.split("--")
-    title, tags = rest.split("__")
+    timestamp: datetime
+    title: str
+    tags: list[str]
 
-    return date, title, tags
+    @classmethod
+    def parse(cls, filename: str) -> Record | None:
+        """Parse an instance of a record from the given filename"""
+
+        if (match := FILENAME_PATTERN.match(filename)) is None:
+            return None
+
+        identifier = match.group("identifier")
+        date, time = identifier.split("T")
+        dt = datetime(
+            year=int(date[:4]),
+            month=int(date[4:6]),
+            day=int(date[6:8]),
+            hour=int(time[:2]),
+            minute=int(time[2:4]),
+            second=int(time[4:6]),
+        )
+
+        slug = match.group("title")
+        tags = match.group("tags").split("_")
+        title = " ".join( c.title() for c in slug.split('-'))
+
+        return cls(
+            identifier=identifier,
+            slug=slug,
+            timestamp=dt,
+            title=title,
+            tags=tags,
+        )
+
+    @property
+    def url(self):
+        """Return the url for this record"""
+
+        if "blog" in self.tags:
+            dirname = pathlib.Path("blog", str(self.timestamp.year))
+            url = str(dirname / self.slug)
+        else:
+            dirname = pathlib.Path("notes")
+            url = str(dirname / self.identifier)
+
+        return url
 
 
 class Denote(Domain):
@@ -62,9 +114,6 @@ class Denote(Domain):
     ) -> Element | None:
         """Resolve cross references"""
 
-        print(self.notes)
-        print(f"resolve: {fromdocname=} {typ=} {target=} {node=} {contnode=}")
-
         if (dest := self.notes.get(target)) is None:
             return None
 
@@ -82,37 +131,35 @@ class DenoteHTMLBuilder(DirectoryHTMLBuilder):
     the web"""
 
     def get_target_uri(self, docname: str, typ: str | None = None) -> str:
-        if (result := parse_denote_name(docname)) is None:
+        domain: Denote = self.env.domains["denote"]
+
+        if (record := domain.notes.get(docname)) is None:
             return super().get_target_uri(docname, typ)
 
-        date, title, tags = result
-
-        dirname = pathlib.Path(docname).parent
-        result = super().get_target_uri(str(dirname / date), typ)
+        result = super().get_target_uri(record.url, typ)
         return result
 
     def get_outfilename(self, pagename: str) -> str:
-        if (result := parse_denote_name(pagename)) is None:
+        domain: Denote = self.env.domains["denote"]
+
+        if (record := domain.notes.get(pagename)) is None:
             return super().get_outfilename(pagename)
 
-        date, title, tags = result
-
-        dirname = pathlib.Path(pagename).parent
-        result = super().get_outfilename(str(dirname / date))
+        result = super().get_outfilename(record.url)
         return result
 
 
 def discover_notes(app: Sphinx, docname: str, content: list[str]):
     """Automatically discover and index notes as they are read"""
 
-    if (result := parse_denote_name(docname)) is None:
+    docpath = pathlib.Path(docname)
+    if (record := Record.parse(docpath.name)) is None:
         return
 
-    date, title, tags = result
     domain: Denote = app.env.domains["denote"]
 
-    domain.notes[date] = note = (docname, title, tags)
-    print(f"found: {note}")
+    logger.info("[denote]: Found record: %r", record)
+    domain.notes[docname] = record
 
 
 def setup(app: Sphinx):
