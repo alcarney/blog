20f3368aa7ea1a077a548d7354df38931d9c302d
Alex Carney <alcarneyme@gmail.com>
2025-07-05 23:57:50 +0100

    project-vc-name note for modeline

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((denote-directory . "/var/home/alex/Projects/alcarney/blog/content"))))
+((nil . ((denote-directory . "/var/home/alex/Projects/alcarney/blog/content")
+         (project-vc-name . "alcarney/blog"))))
